253cb86689f9fe924d1a26f4b8690f2aff7f20ce
AishaMJ <aishamj456@gmail.com>
2018-10-27 14:49:32 +0530

    Added logic

--- aishaa.c
+++ aishaa.c
@@ -1,11 +1,22 @@
 #include<studio.h>
+
 void main()
 {
+
 	int x=10;
+
 	int y=5;
-	printf("\n values before swapping");
+	
+printf("\n values before swapping");
+
 	printf("\n x:%d\n y:%\n",x,y);
 
+int temp=x;
+x=y;
+y=temp;
+
 	printf("\n values after swapping");
-	printf("\n x: %d\n y:%d\n,x,y);
+	
+printf("\n x: %d\n y:%d\n,x,y);
+
 }
