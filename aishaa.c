6f1e5f32eff33c8ebad73d5559b0170fee06917e
AishaMJ <aishamj456@gmail.com>
2018-10-27 14:44:58 +0530

    test

---
+++ aishaa.c
@@ -0,0 +1,11 @@
+#include<studio.h>
+void main()
+{
+	int x=10;
+	int y=5;
+	printf("\n values before swapping");
+	printf("\n x:%d\n y:%\n",x,y);
+
+	printf("\n values after swapping");
+	printf("\n x: %d\n y:%d\n,x,y);
+}
